d409c3fa3d1a9eae67ed841988b83286b2722646
saradonnelly97 <sara.donnelly8@gmail.com>
2023-05-31 23:26:27 -0400

    revisit

--- public/index.js
+++ public/index.js
@@ -2,9 +2,12 @@ function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+    async function getWebpage() { 
+        let response = await fetch('http://127.0.0.1:9000/')
+        console.log(response)
+        }
 
     let countValue = 0;
-
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
@@ -17,6 +20,8 @@ function main(){
 
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+
+
     countContainer.textContent = countValue;
 }
 main()
